16b89cdb516c98d1753a0c9516db2911d3391c82
心飞为你飞 <17671787093@163.com>
2026-08-10 21:01:27 +0800

    feat: 首页添加搜索功能，支持跳转到搜索结果页

--- pinyougou-portal-web/src/main/webapp/js/controller/contentController.js
+++ pinyougou-portal-web/src/main/webapp/js/controller/contentController.js
@@ -1,13 +1,22 @@
 app.controller('contentController',function($scope,contentService){
-	
+
 	$scope.contentList=[];//广告列表
-	
+
 	$scope.findByCategoryId=function(categoryId){
 		contentService.findByCategoryId(categoryId).success(
 			function(response){
 				$scope.contentList[categoryId]=response;
 			}
-		);		
+		);
 	}
-	
+
+	// 搜索功能
+	$scope.keywords='';
+	$scope.search=function(){
+		if($scope.keywords && $scope.keywords.trim()){
+			// 跳转到搜索页面
+			location.href='../search-web/search.html#?keywords='+encodeURIComponent($scope.keywords.trim());
+		}
+	}
+
 });
